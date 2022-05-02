fe24bba7bec754eae67d7aded3389409a9f5b054
Agu Valeriani <avaleriani@users.noreply.github.com>
2022-05-02 15:18:17 +0200

    - removed extra /

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 const CONSTANTS = {
   POOLS_URL: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
-  TOKEN_LOGO_URL: "https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/128/icon/",
+  TOKEN_LOGO_URL: "https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/128/icon",
   LOGO_NOT_AVAILABLE_URL: "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg",
   ITEMS_PER_PAGE: 10,
   ETHERSCAN_TX_URL: "https://etherscan.io/tx",
