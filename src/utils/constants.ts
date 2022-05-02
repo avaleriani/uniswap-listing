e259deb20692c77d15f00439410f45f43fb596c4
Agu Valeriani <avaleriani@users.noreply.github.com>
2022-05-02 14:54:59 +0200

    update icons constant url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 const CONSTANTS = {
   POOLS_URL: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
-  TOKEN_LOGO_URL: "https://api.coinicons.net/icon",
+  TOKEN_LOGO_URL: "https://raw.githubusercontent.com/condacore/cryptocurrency-icons/master",
   LOGO_NOT_AVAILABLE_URL: "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg",
   ITEMS_PER_PAGE: 10,
   ETHERSCAN_TX_URL: "https://etherscan.io/tx",
