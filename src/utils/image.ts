a663c82a217e1c1d6792f53eb7d687a1bee6c211
Agu Valeriani <avaleriani@users.noreply.github.com>
2022-05-02 16:22:47 +0200

    - another icons source test

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,2 +1,4 @@
 import CONSTANTS from "utils/constants";
-export const getTokenLogoUrl = (symbol: string) => `${CONSTANTS.TOKEN_LOGO_URL}/${symbol?.toLowerCase()}.png`;
+
+const camelCase = (str) => str.replace(/[A-Z]/g, m => "-" + m.toLowerCase());
+export const getTokenLogoUrl = (symbol: string) => `${CONSTANTS.TOKEN_LOGO_URL}/${camelCase(symbol)}.png`;
