4eb9f7ffb4b226cff2a240a6c3e16a3ee7ce05d3
Agu Valeriani <avaleriani@users.noreply.github.com>
2022-05-02 17:10:30 +0200

    -fix typing issue

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -3,7 +3,7 @@ import CONSTANTS from "utils/constants";
 const toDashCase = (str = ``) => {
     let result = '';
     for(let item of [...str]) {
-        if(item.charCodeAt() > 'a'.charCodeAt() || !Number.isNaN(+item)) {
+        if(item.charCodeAt(0) > 'a'.charCodeAt(0) || !Number.isNaN(+item)) {
             result += item;
         } else {
             result += `-${item.toLocaleLowerCase()}`;
