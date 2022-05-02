a45c1ed8fedd9d6dfcc5e84990fcdf563c9e95de
Agu Valeriani <avaleriani@users.noreply.github.com>
2022-05-02 17:21:58 +0200

    - replaced todashcase function, simpler now

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,15 +1,5 @@
 import CONSTANTS from "utils/constants";
 
-const toDashCase = (str = ``) => {
-    let result = '';
-    for(let item of [...str]) {
-        if(item.charCodeAt(0) > 'a'.charCodeAt(0) || !Number.isNaN(+item)) {
-            result += item;
-        } else {
-            result += `-${item.toLocaleLowerCase()}`;
-        }
-    }
-    return result;
-}
+const toDashCase = (str = ``) => str.replace(/\s+/g, '-').toLowerCase();
 
 export const getTokenLogoUrl = (symbol: string) => `${CONSTANTS.TOKEN_LOGO_URL}/${toDashCase(symbol)}.png`;
