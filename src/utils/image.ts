cdb53a6b37a538d3672503445e68339b474c049b
Agu Valeriani <avaleriani@users.noreply.github.com>
2022-05-02 14:54:36 +0200

    delete string

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,3 +1,2 @@
 import CONSTANTS from "utils/constants";
-https://raw.githubusercontent.com/condacore/cryptocurrency-icons/master/64x64/bitcoin.png
 export const getTokenLogoUrl = (symbol: string, size = 64) => `${CONSTANTS.TOKEN_LOGO_URL}/${size}x${size}/${symbol}.png`;
